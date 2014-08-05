c8fd7596fea5bec808242e7369a6f60e38ab5ec8
last1here <last1here@hotmail.co.uk>
2014-08-05 22:05:26 +0100

    basic file layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,57 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		cssmin: {
+      options: {
+        compatibility: 'ie8',
+        keepSpecialComments: '*',
+        noAdvanced: true
+      },
+      minify: {
+    		expand: true,
+    		cwd: 'dist/',
+    		src: ['*.css', '!*.min.css'],
+    		dest: 'dist/min/',
+    		ext: '.min.css'
+ 			}
+    },
+    csscomb: {
+      options: {
+        config: 'scss/.csscomb.json'
+      },
+      dist: {
+        expand: true,
+        cwd: 'dist/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'dist/'
+      }
+    },
 		sass: {
-			dist: {
-				files: {
-					'dist/style.css' : 'scss/style.scss'
-				}
-			}
+			dist: {                            // Target
+      	options: {                       // Target options
+      	 	style: 'expanded',
+      	 	banner: '/*!\n' +
+            ' * laySCSS v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2014-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' */\n',
+      	},
+      	files: {                         // Dictionary of files
+        	'dist/<%= pkg.name %>.css': 'scss/<%= pkg.name %>.scss'
+      	}
+   		}
 		},
 		watch: {
 			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
+				files: 'scss/**/*.scss',
+				tasks: ['sass', 'csscomb', 'cssmin']
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-csscomb');
+
 	grunt.registerTask('default',['watch']);
 }
